fff31a3e51bd720b0e2d54bdb2038b0a01c7c9e5
Protiuc Arteom <protsyuk.artem@bk.ru>
2021-01-23 10:46:15 +0200

    Next

--- Dom/app.js
+++ Dom/app.js
@@ -3,3 +3,7 @@ document.body.style.background = 'red';
 //Sarc2
 setTimeout(() => document.body.style.background = 'green', 3000);
 //Sarc3
+function changeBodyBg(){
+    document.body.style.background = 'orange';
+}
+//Sarc4
